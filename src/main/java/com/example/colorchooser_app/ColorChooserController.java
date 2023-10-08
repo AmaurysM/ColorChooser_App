0f5aaf8cc385f605f847f05a3aee7b589eda89fb
Amaurys De Los Santos <amaurysdlsm@gmail.com>
2023-10-07 21:24:30 -0400

    First Commit

--- src/main/java/com/example/colorchooser_app/ColorChooserController.java
+++ src/main/java/com/example/colorchooser_app/ColorChooserController.java
@@ -1,14 +1,73 @@
 package com.example.colorchooser_app;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class ColorChooserController {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Slider AlphaSlider;
+
+    @FXML
+    private TextField AlphaTextField;
+
+    @FXML
+    private Slider BlueSlider;
+
+    @FXML
+    private TextField BlueTextField;
+
+    @FXML
+    private Pane ColorPickerPane;
+
+    @FXML
+    private Slider GreenSlider;
+
+    @FXML
+    private TextField GreenTextField;
+
+    @FXML
+    private Slider RedSlider;
+
+    @FXML
+    private TextField RedTextField;
+    @FXML
+    private void RedsliderMoved(MouseEvent event){
+        RedTextField.setText(String.valueOf(  (Math.round((RedSlider.getValue())*100.00))/100.00 ));
+        changeColor(event);
     }
+    @FXML
+    private void GreensliderMoved(MouseEvent event){
+        GreenTextField.setText(String.valueOf(  (Math.round((GreenSlider.getValue())*100.00))/100.00 ));
+        changeColor(event);
+    }
+    @FXML
+    private void BluesliderMoved(MouseEvent event){
+        BlueTextField.setText(String.valueOf(  (Math.round((BlueSlider.getValue())*100.00))/100.00 ));
+        changeColor(event);
+    }
+    @FXML
+    private void AlphasliderMoved(MouseEvent event){
+        AlphaTextField.setText(String.valueOf(  (Math.round((AlphaSlider.getValue())*100.00))/100.00 ));
+        changeColor(event);
+    }
+
+    @FXML
+    private void changeColor(MouseEvent event){//new Color(10,20,20,20)
+        try {
+            ColorPickerPane.setBackground(Background.fill(new Color(Double.valueOf(RedTextField.getText()),
+                    Double.valueOf(GreenTextField.getText()),
+                    Double.valueOf(BlueTextField.getText()),
+                    Double.valueOf(AlphaTextField.getText()))));
+        }catch (NumberFormatException ex){
+
+        }
+
+    }
+
 }
